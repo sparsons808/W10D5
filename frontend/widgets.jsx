800c66755d1536310db32e8fc517c16f82848025
your name hereEddy_Marshall <epmarsha2@gmail.com>
2021-08-20 16:03:28 -0400

    finalize project setup

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Congrats from '../congrats';
+import Clock from '../clock';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Congrats/>, root);
+  ReactDOM.render(<Clock/>, root);
 });
